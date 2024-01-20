502372e615370b16b85036ac0342914d831be8c3
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2024-01-20 18:17:40 +0100

    cleanup

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from chessapp.chessapp import ChessApp
 from chessapp.model.chesstree import ChessTree
 import sys
 from chessapp.util.paths import get_openings_folder
+from chessapp.view.pieces import load_pieces
 
 tree = ChessTree(get_openings_folder())
 tree.load()
 qtapp = ChessApp(tree, sys.argv)
 qtapp.aboutToQuit.connect(qtapp.close)
+load_pieces()
 qtapp.exec_()
