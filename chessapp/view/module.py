961d21f47f564d9a3ba29b3176dddd23521da7be
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2024-01-20 18:20:19 +0100

    abstraction of different module types

--- chessapp/view/module.py
+++ chessapp/view/module.py
@@ -1,35 +1,26 @@
 from PyQt5.QtCore import QRunnable, QObject, Qt
 from PyQt5.QtWidgets import QAction, QMenuBar, QMenu, QWidget, QListWidget, QHBoxLayout, QLabel, QVBoxLayout, QListWidgetItem
 from PyQt5.QtGui import QFont
-from chessapp.view.chessboardwidget import ChessBoardWidget
-from chessapp.view.piecemovement import PieceMovement
+from chessapp.view.chessboardwidget import ChessBoardWidget, PieceMovement
 from PyQt5.QtCore import pyqtSignal
 from chessapp.configuration import DEFAULT_STYLESHEET
 
 
-class Module(QObject):
-
-    log_message_received = pyqtSignal(str)
-
-    def __init__(self, app, display_name: str, actions: [QAction]):
+class BaseModule(QObject):
+    def __init__(self, app, display_name: str, actions: [QAction]) -> None:
         super().__init__()
         self.app = app
         self.display_name: str = display_name
         self.is_focused = False
         self.central_widget = QWidget()
-        self.center_widget = QWidget()
-        self.log_widget = QListWidget()
-        self.chess_board_widget = ChessBoardWidget()
+        self.main_widget = QWidget()
         self.header_label = QLabel()
         self.footer_label = QLabel()
-        self.chess_board_widget.piece_movement_observer.append(
-            self.on_piece_movement)
         self.actions: [QAction] = actions
-        self.init()
         self.is_closing = False
         self.is_closed = False
 
-    def init(self):
+    def init(self) -> None:
         # widget configuration
         self.header_label.setAlignment(Qt.AlignCenter)
         self.header_label.setFont(QFont("Times", 20, QFont.Bold))
@@ -37,24 +28,15 @@ class Module(QObject):
         self.central_widget.setStyleSheet(
             self.central_widget.styleSheet() + ";" + DEFAULT_STYLESHEET)
 
-        # top, centre, bottom
+        # header, main, footer
         v_layout = QVBoxLayout()
         self.central_widget.setLayout(v_layout)
         v_layout.addWidget(self.header_label)
-        v_layout.addWidget(self.center_widget)
+        v_layout.addWidget(self.main_widget)
         v_layout.addWidget(self.footer_label)
-
-        # centre: left chess board, right log
-        h_Layout = QHBoxLayout()
-        self.center_widget.setLayout(h_Layout)
-        h_Layout.addWidget(self.chess_board_widget)
-        h_Layout.addWidget(self.log_widget)
 
         # default values
         self.header_label.setText(self.display_name)
-
-        # connections
-        self.log_message_received.connect(self.__log_message)
 
     def register(self):
         menu_bar: QMenuBar = self.app.window.menuBar()
@@ -73,9 +55,6 @@ class Module(QObject):
     def on_register(self):
         pass
 
-    def on_piece_movement(self, piece_movement: PieceMovement):
-        pass
-
     def on_close(self):
         pass
 
@@ -88,20 +67,11 @@ class Module(QObject):
         self.is_closed = True
         self.is_closing = False
 
-    def log_message(self, message: str, timeout_milliseconds: int = 2000):
-        self.log_message_received.emit(message)
-        self.app.show_status_message(message, timeout_milliseconds)
-
-    def __log_message(self, message: str):
-        QListWidgetItem(message, self.log_widget)
-        self.log_widget.scrollToBottom()
-
     def focus(self):
         if self.is_focused:
             return
         self.app.unfocus_all_modules()
         self.app.set_central_widget(self.central_widget)
-        self.chess_board_widget.grabKeyboard()
         self.is_focused = True
         self.on_focus()
 
@@ -119,6 +89,60 @@ class Module(QObject):
 
     def dispatch_threadpool(self, callable):
         self.app.threadpool.start(MethodAction(callable))
+
+
+class LogModule(BaseModule):
+
+    log_message_received = pyqtSignal(str)
+
+    def __init__(self, app, display_name: str, actions: [QAction]):
+        super().__init__(app, display_name, actions)
+        self.log_widget = QListWidget()
+
+    def init(self):
+        super().init()
+        # main: log
+        v_Layout = QVBoxLayout()
+        self.main_widget.setLayout(v_Layout)
+        v_Layout.addWidget(self.log_widget)
+        # connections
+        self.log_message_received.connect(self.__log_message)
+
+    def log_message(self, message: str, timeout_milliseconds: int = 2000):
+        self.log_message_received.emit(message)
+        self.app.show_status_message(message, timeout_milliseconds)
+
+    def __log_message(self, message: str):
+        QListWidgetItem(message, self.log_widget)
+        self.log_widget.scrollToBottom()
+
+
+class ChessboardAndLogModule(LogModule):
+
+    def __init__(self, app, display_name: str, actions: [QAction]):
+        super().__init__(app, display_name, actions)
+        self.chess_board_widget = ChessBoardWidget()
+        self.chess_board_widget.piece_movement_observer.append(
+            self.on_piece_movement)
+
+    def init(self):
+        super().init()
+        # reset main widget because it is used differently in LogModule. also reset parent of log widget so that it is not deleted when
+        # the main widget is deleted/removed as a child
+        self.log_widget.setParent(None)
+        QWidget().setLayout(self.main_widget.layout())
+        # main: left chess board, right log
+        h_Layout = QHBoxLayout()
+        self.main_widget.setLayout(h_Layout)
+        h_Layout.addWidget(self.chess_board_widget)
+        h_Layout.addWidget(self.log_widget)
+
+    def on_piece_movement(self, piece_movement: PieceMovement):
+        pass
+
+    def on_focus(self):
+        super().on_focus()
+        self.chess_board_widget.grabKeyboard()
 
 
 class MethodAction(QRunnable):
