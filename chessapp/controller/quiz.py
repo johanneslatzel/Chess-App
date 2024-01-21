64483626a9c2404554dfe967fa2b4eab92a5cb9c
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2024-01-21 13:45:31 +0100

    documentation

--- chessapp/controller/quiz.py
+++ chessapp/controller/quiz.py
@@ -14,12 +14,26 @@ from chessapp.model.node import Node
 from chessapp.sound.chessboardsound import ChessboardSound
 
 s_starting_position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -"
-s_quiz_start_at_fen = s_starting_position
 
 
 class Quiz(ChessboardAndLogModule):
+    """ this module is an interactive quiz in which the player has to find acceptable moves (given by the position evaluations) for given positions.
+    usually the opening tree of the opening tree module (@see chessapp.controller.OpeningTree) is used to find opponent moves played with a
+    probability that is in proportion to the statistical occurence of that move given the specific players the opening tree is based on. if no move
+    is known for the given position in the opening tree then a random move is chosen from the tree that is given during module initialization.
+    the quiz ends when a no more moves are known for a certain positon.
+    """
 
     def __init__(self, app, tree: ChessTree, opening_tree: OpeningTree, explorer: Explorer):
+        """initializes the quiz module. the actions are start, reset and explore. explore opens the explorer module (@see chessapp.controller.Explorer)
+        and sets the board to the current position. start starts the quiz. reset resets the board to the starting position.
+
+        Args:
+            app (ChessApp): the main application
+            tree (ChessTree): the tree to use for the quiz
+            opening_tree (OpeningTree): the opening tree to use for the quiz
+            explorer (Explorer): the explorer module
+        """
         super().__init__(app, "Quiz", [create_method_action(app, "Start", self.start), create_method_action(
             app, "Reset", self.reset), create_method_action(app, "Explore", self.explore)])
         self.tree: ChessTree = tree
@@ -33,6 +47,16 @@ class Quiz(ChessboardAndLogModule):
         random.seed()
 
     def apply_movement(self, piece_movement: PieceMovement):
+        """ this method is called when the user makes a move on the chessboard. it checks if the move is acceptable and if so applies it to the board. 
+        if the move is acceptable it will call apply_opponent_move to apply the opponents move. if the move is not acceptable it will log a message.
+        also if the move is unknown it will be added to the opening tree (and can be explored later).
+
+        Args:
+            piece_movement (PieceMovement): the movement to apply
+
+        Raises:
+            Exception: if this method is called during an opponents move
+        """
         if not self.quiz_started:
             return
         if not self.player_turn:
@@ -70,6 +94,12 @@ class Quiz(ChessboardAndLogModule):
         self.apply_opponent_move()
 
     def apply_opponent_move(self):
+        """ this method is called when the opponent makes a move. it will choose a move from the opening tree (if known) by proportion of known moves
+        or a random move otherwise (known by the given tree). if there is no more move known then the quiz stops.
+
+        Raises:
+            Exception: if this method is called during the players turn
+        """
         if not self.quiz_started:
             return
         if self.player_turn:
@@ -108,7 +138,16 @@ class Quiz(ChessboardAndLogModule):
             self.finish_quiz(
                 node, "opponent moved, no more acceptable moves for player known")
 
-    def finish_quiz(self: str, node: Node, reason):
+    def finish_quiz(self, node: Node, reason: str):
+        """ this method is called when the quiz is finished. it will log the reason of termination, the moves played and the moves left in the node (if any).
+
+        Args:
+            node (Node): the node that that was reached when the quiz was finished
+            reason (str): the reason why the quiz was finished
+
+        Raises:
+            Exception: if the quiz has not been started yet
+        """
         if not self.quiz_started:
             raise Exception("cannot finish quiz that has not been started yet")
         self.log_message("quiz finished with reason: " + reason)
@@ -125,19 +164,31 @@ class Quiz(ChessboardAndLogModule):
         ChessboardSound.GAME_END.play()
 
     def on_piece_movement(self, piece_movement: PieceMovement):
+        """ this method is called when the user makes a move on the chessboard. it will dispatch apply_movement for this piece movement on the
+        threadpool of the main application (because this method is called from the gui thread and apply_movement may take some time to complete).
+
+        Args:
+            piece_movement (PieceMovement): the movement to apply
+        """
         if not self.about_to_close():
             self.app.threadpool.start(QuizMovementAction(self, piece_movement))
 
     def reset(self):
+        """ this method resets the board to the starting position and resets the quiz state.
+        """
         self.quiz_started = False
         self.board = Board()
         self.chess_board_widget.display(self.board)
 
     def explore(self):
+        """ opens the explorer module and sets the board of the explorer to the board of the quiz.
+        """
         self.explorer.focus()
         self.explorer.set_board(self.board)
 
     def start(self):
+        """ this method starts the quiz. it will randomly choose a color for the player and displays the board from the perspective of the player.
+        """
         if self.quiz_started:
             return
         self.focus()
@@ -157,16 +208,37 @@ class Quiz(ChessboardAndLogModule):
 
 
 class QuizMovementAction(QRunnable):
+    """ this class is a runnable that is used to apply a piece movement to the quiz (@see on_piece_movement). it is used to run apply_movement
+    on the threadpool of the main application.
+    """
+
     def __init__(self, quiz: Quiz, piece_movement: PieceMovement):
+        """initializes the runnable
+
+        Args:
+            quiz (Quiz): the quiz to apply the movement to
+            piece_movement (PieceMovement): the movement to apply
+        """
         super().__init__()
         self.quiz = quiz
         self.piece_movement = piece_movement
 
     def run(self):
+        """ applies the movement to the quiz
+        """
         self.quiz.apply_movement(self.piece_movement)
 
 
 def moves_to_pgn(moves, white_first_move: bool) -> str:
+    """ this method converts a list of moves to a pgn string
+
+    Args:
+        moves ([str]): array of moves as strings
+        white_first_move (bool): whether the first move is a move of the player with the white pieces
+
+    Returns:
+        str: the pgn string
+    """
     pgn = ""
     for i in range(0, len(moves)):
         if (i % 2 == 0 and white_first_move) or (i % 2 != 0 and not white_first_move):
