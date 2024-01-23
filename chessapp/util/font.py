1aa2f0c89b365cb574678d1f9d3ec228dd471cc6
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2024-01-23 17:34:01 +0100

    draw eval value on evalbar

--- chessapp/util/font.py
+++ chessapp/util/font.py
@@ -1,11 +1,11 @@
-from PyQt5.QtGui import QFont, QFontMetrics
+from PyQt5.QtGui import QFont, QFontMetrics, QPainter
 from PyQt5.QtCore import QRect, QSize
 
-s_min_font_size = 10
+s_min_font_size = 2
 s_max_font_size = 100
 
 
-def find_font_size(bounding_box: QSize, text: str):
+def find_font_size(bounding_box: QSize, text: str, qp: QPainter = None) -> int:
     """finds the best font size for the given text and bounding box to fill the box with the given text. the minimum font size is
     s_min_font_size and the maximum font size is s_max_font_size. the font size is returned as the maximum lower bound thats fits
     the text inside the box (unless the text is too large for the box even with the minimum font size applied) then the minimum
@@ -13,12 +13,16 @@ def find_font_size(bounding_box: QSize, text: str):
 
     Args:
         bounding_box (QSize): bounding box of the text
-        text (str): _description_
+        bounding_box (QPainter, optional): QPainter of the GUI. Defaults to None.
+        text (str): text to fit inside the bounding box
 
     Returns:
-        _type_: _description_
+        the font size that fits the text inside the bounding box in PointSize
     """
-    font = QFont()
+    if qp:
+        font = qp.font()
+    else:
+        font = QFont()
     low = s_min_font_size
     high = s_max_font_size
     # binary search for the best font size
@@ -27,7 +31,7 @@ def find_font_size(bounding_box: QSize, text: str):
         font.setPointSize(mid)
         metrics = QFontMetrics(font)
         bounding_font_rect: QRect = metrics.boundingRect(text)
-        if bounding_font_rect.width() > bounding_box.width() or bounding_font_rect.height() > bounding_box.height():
+        if bounding_font_rect.width() >= bounding_box.width() or bounding_font_rect.height() >= bounding_box.height():
             high = mid
         else:
             low = mid
--- chessapp/view/evalbar.py
+++ chessapp/view/evalbar.py
@@ -1,11 +1,14 @@
 from PyQt5.QtGui import QPainter
-from PyQt5.QtCore import QRect
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QRect, QSize, Qt
 from chessapp.model.node import Node
 from chessapp.configuration import MAX_EVALBAR_VALUE_ABS
+from chessapp.util.font import find_font_size
 
 
 class EvalBar():
+    """ This class represents the evalbar of a chessboard. It is drawn on the left side of the chessboard and shows the evaluation of the current position.
+    The evaluation is shown by a bar with coloring proportionally to the evaluation. In the center of the bar the evaluation is shown as text.
+    """
 
     def __init__(self):
         self.width = 0
@@ -17,6 +20,7 @@ class EvalBar():
         self.is_flipped = not self.is_flipped
 
     def drawOn(self, qp: QPainter, bound: QRect):
+        # draw evalbar itself
         second_color_height_percentage = (
             MAX_EVALBAR_VALUE_ABS - self.node.eval) / (2 * MAX_EVALBAR_VALUE_ABS)
         if self.is_flipped:
@@ -25,3 +29,15 @@ class EvalBar():
         ), Qt.GlobalColor.black if self.is_flipped else Qt.GlobalColor.white)
         qp.fillRect(bound.x(), bound.y(), bound.width(), int(second_color_height_percentage *
                     bound.height()), Qt.GlobalColor.white if self.is_flipped else Qt.GlobalColor.black)
+        # draw evalbar text
+        font = qp.font()
+        eval_text: str = str(self.node.eval)
+        if self.node.eval > 0:
+            eval_text = "+" + eval_text
+        elif self.node.eval == 0:
+            eval_text = "=0.00"
+        size = find_font_size(
+            QSize(bound.width(), bound.height()), eval_text)
+        font.setPointSize(size)
+        qp.setFont(font)
+        qp.drawText(bound, Qt.AlignCenter, eval_text)
