036bf980a9c3cdecbdd9de6fe0c61fecec4b180a
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2024-01-23 18:10:15 +0100

    documentation

--- chessapp/view/evalbar.py
+++ chessapp/view/evalbar.py
@@ -6,20 +6,34 @@ from chessapp.util.font import find_font_size
 
 
 class EvalBar():
-    """ This class represents the evalbar of a chessboard. It is drawn on the left side of the chessboard and shows the evaluation of the current position.
-    The evaluation is shown by a bar with coloring proportionally to the evaluation. In the center of the bar the evaluation is shown as text.
+    """ This class represents the evalbar of a chessboard. It is drawn on the left side of the chessboard
+    and shows the evaluation of the current position. The evaluation is shown by a bar with coloring
+    proportionally to the evaluation. In the center of the bar the evaluation is shown as text. The size of
+    the text is chosen so that it fits into the bar (scaling fits). The bar is usually flipped if the chessboard
+    gets flipped.
     """
 
     def __init__(self):
+        """ Initializes the evalbar.
+        """
         self.width = 0
         self.is_visible = False
         self.node: Node = None
         self.is_flipped = False
 
     def flip(self):
+        """ Flips the evalbar. If the evalbar is flipped then white is on top and black on bottom. otherwise
+        black is on top and white on bottom.
+        """
         self.is_flipped = not self.is_flipped
 
     def drawOn(self, qp: QPainter, bound: QRect):
+        """ Draws the evalbar on the given painter within the given bounds.
+
+        Args:
+            qp (QPainter): the painter to draw on
+            bound (QRect): the bounds to draw in
+        """
         # draw evalbar itself
         second_color_height_percentage = (
             MAX_EVALBAR_VALUE_ABS - self.node.eval) / (2 * MAX_EVALBAR_VALUE_ABS)
