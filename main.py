5b6362f5fd367343738c28fc7b50fed7d7cb9a02
Johannes B. Latzel <smartphone.johannes.latzel@outlook.de>
2024-02-17 16:32:43 +0100

    material style

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from chessapp.model.chesstree import ChessTree
 import sys
 from chessapp.util.paths import get_openings_folder
 from chessapp.view.pieces import load_pieces
+from qt_material import apply_stylesheet
 
 tree = ChessTree(get_openings_folder())
 tree.load()
 qtapp = ChessApp(tree, sys.argv)
 qtapp.aboutToQuit.connect(qtapp.close)
+apply_stylesheet(qtapp, theme='dark_teal.xml')
 load_pieces()
 qtapp.exec_()
